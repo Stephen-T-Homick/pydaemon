02fa7a3e8c2956812a563b5df21c54d0fe0adafd
Stephen-T-Homick <shomick@datto.com>
2018-03-16 14:44:39 -0400

    Initial arg parsing.

--- imdeamon.py
+++ imdeamon.py
@@ -15,9 +15,6 @@ import time
 
 
 
-
-
-
 # Initialize logger subsets / locations
 logger = logging.getLogger('imdaemon')
 
@@ -25,7 +22,6 @@ logger = logging.getLogger('imdaemon')
 # CLI Argument Parsing
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--pidfile', help='.', required = True)
-parser.add_argument('--verbose', help='Increases Verbosity of the script / daemon. Look for more redirection to syslog as well.', action='store_true')
-parser.add_argument('--host', help='Remote host against which to run. Default: netbox.datto.net')
-parser.add_argument('--sleeptime', help='For how long to sleep between loops')
+parser.add_argument('--daemonize', help='Runs this in background as a "true" daemon - uninterruptible', required = True)
+parser.add_argument('-v', help='Increases Verbosity of the script / daemon. Look for more redirection to syslog as well.', action='store_true')
+parser.add_argument('--sleeptime', help='For how long to sleep between daemonization cycles.')
