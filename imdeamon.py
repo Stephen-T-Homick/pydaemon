238ad39f8403cbcb1f0cade3d25f87769a9c66f0
Stephen-T-Homick <shomick@datto.com>
2018-03-19 14:06:40 -0400

    Staged.

--- imdeamon.py
+++ imdeamon.py
@@ -30,10 +30,10 @@ log_to_console.setLevel(logging.DEBUG)
 # CLI Argument Parsing
 
 
-parser = argparse.ArgumentParser(description = 'This is a light weight daemon to run background processes.')
-parser.add_argument('--daemonize', help='Runs this in background as a "true" daemon - uninterruptible', required = True)
+parser = argparse.ArgumentParser(description = 'This is a light weight daemon to demonstrate a syslog server over UDP which inherits from a syslog client.')
+parser.add_argument('--daemonize', help='Runs this in background as a "true" daemon - uninterruptible', required = False)
 parser.add_argument('-help', action='help', help="Show this help message")
-parser.add_argument('--port', action='help', help="Port of which to listen on. Default will be 33884")
+parser.add_argument('--port', help='Port of which to listen on. Default will be 33884', required = True)
 parser.add_argument('--stdout', help="Adds daemon processes to stdout. Off by default.")
 parser.add_argument('-v', help='Increases Verbosity of the script / daemon. Look for more redirection to syslog as well.', action='store_true')
 parser.add_argument('--sleeptime', help='For how long to sleep between daemonization cycles.')
