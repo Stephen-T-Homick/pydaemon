021e96c0ff96fa7925adb66553a0cc9bc49c75a8
Stephen-T-Homick <shomick@datto.com>
2018-03-19 14:21:05 -0400

    Staging

--- imdeamon.py
+++ imdeamon.py
@@ -27,18 +27,13 @@ log_to_console = logging.StreamHandler()
 log_to_console.setLevel(logging.DEBUG)
 """
 # CLI Argument Parsing
-
-
 parser = argparse.ArgumentParser(description = 'This is a light weight daemon to demonstrate a syslog server / daemon over UDP which inherits from a syslog client.')
-parser.add_argument('--daemonize', help='Runs this in background as a "true" daemon - uninterruptible', required = False)
-parser.add_argument('-help', action='help', help="Show this help message")
-parser.add_argument('--port', help='Port of which to listen on. Default will be 33884', required = True)
-paser.ArgumentError('')
+parser.add_argument('--daemonize', help='Runs this in background as a "true" syslog daemon - uninterruptible', required = False)
+parser.add_argument('-help', action='help', help="Show this help message, and exit.")
+parser.add_argument('--port', help='Port of which to listen on. Default will be UDP 514 (rsyslog)', required = False)
 parser.add_argument('--stdout', help="Adds daemon processes to stdout. Off by default.")
 parser.add_argument('-v', help='Increases Verbosity of the script / daemon. Look for more redirection to syslog as well.', action='store_true')
-parser.add_argument('--sleeptime', help='For how long to sleep between daemonization cycles.')
+parser.add_argument('--sleep', help='For how long to sleep between daemonization cycles. Example - python imdeamon.py --sleep 5m')
 parser.add_argument('--zombie', help='Become zombie process')
 
 args = parser.parse_args()
-if args.daemonize and not args.port:
-    print "Port option must be enabled."
