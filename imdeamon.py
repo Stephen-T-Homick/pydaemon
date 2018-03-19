f99f114277449bffbe699f3e7b2092628a3ca5ce
Stephen-T-Homick <shomick@datto.com>
2018-03-19 14:56:10 -0400

    Staging.

--- imdeamon.py
+++ imdeamon.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 """Steve Homick
 2018
-imdaemon.py"""
+imdaemon.py
+"""
 import argparse
 import json
 import logging
@@ -20,12 +21,7 @@ syslogSuccessCodes = ['']
 
 logging.basicConfig(level=logging.INFO, format='%(message)s', datefmt='', filename=LOG_FILE, filemode = 'a')
 
-# Begin daemonization by first creating a class for Syslog UDP Handling, then a function to handle the incoming data.
-class SyslogUDPHandler(SocketServer.BaseRequestHandler):
-
-    def handle_request(self):
-        date = bytes.decode(self.request[0].strip())
-
+# Begin daemonization
 
 
 """
@@ -44,11 +40,7 @@ log_to_console.setLevel(logging.DEBUG)
 parser = argparse.ArgumentParser(description = 'This is a light weight daemon to demonstrate a syslog server / daemon over UDP which inherits from a syslog client.')
 parser.add_argument('--daemonize', help='Runs this in background as a "true" syslog daemon - uninterruptible.', required = False)
 parser.add_argument('-help', action='help', help="Show this help message, and exit.")
-parser.add_argument('--port', help='Port of which to listen on. Default will be UDP 514 (rsyslog) unless otherwise specified.', required = False)
-parser.add_argument('--stdout', help="Adds daemon processes to stdout. Off by default.")
 parser.add_argument('-v', help='Increases Verbosity of the script / daemon. Look for more redirection to syslog as well.', action='store_true')
-parser.add_argument('--sleep', help='For how long to sleep between daemonization cycles. Example - python imdeamon.py --sleep 5m')
-parser.add_argument('--zombie', help='Become zombie process')
 
 
 args = parser.parse_args()
