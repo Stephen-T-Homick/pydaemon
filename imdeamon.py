a5805e8295c4d6f92b6bfab7f5c36507e5842328
Stephen-T-Homick <shomick@datto.com>
2018-03-19 14:10:43 -0400

    Staged

--- imdeamon.py
+++ imdeamon.py
@@ -14,8 +14,7 @@ import time
 #    print "I'm a daemon, I need root privileges. \n \n Exiting."
 #    sys.exit(1)
 
-httpSuccessCodes = [200,201,202,204]
-
+syslogSuccessCodes = ['']
 
 """
 # Initialize logger subsets / locations
@@ -30,13 +29,16 @@ log_to_console.setLevel(logging.DEBUG)
 # CLI Argument Parsing
 
 
-parser = argparse.ArgumentParser(description = 'This is a light weight daemon to demonstrate a syslog server over UDP which inherits from a syslog client.')
+parser = argparse.ArgumentParser(description = 'This is a light weight daemon to demonstrate a syslog server / daemon over UDP which inherits from a syslog client.')
 parser.add_argument('--daemonize', help='Runs this in background as a "true" daemon - uninterruptible', required = False)
 parser.add_argument('-help', action='help', help="Show this help message")
 parser.add_argument('--port', help='Port of which to listen on. Default will be 33884', required = True)
+paser.ArgumentError('')
 parser.add_argument('--stdout', help="Adds daemon processes to stdout. Off by default.")
 parser.add_argument('-v', help='Increases Verbosity of the script / daemon. Look for more redirection to syslog as well.', action='store_true')
 parser.add_argument('--sleeptime', help='For how long to sleep between daemonization cycles.')
 parser.add_argument('--zombie', help='Become zombie process')
 
 args = parser.parse_args()
+if args.daemonize and not args.port:
+    print "Port option must be enabled."
