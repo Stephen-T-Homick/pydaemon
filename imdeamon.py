fca320f51ac11975e097e7f8d2edfc5901bbcc20
Stephen-T-Homick <shomick87@gmail.com>
2018-04-04 21:15:40 -0400

    Edited. Working daemonization started.

--- imdeamon.py
+++ imdeamon.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
-"""Steve Homick
-2018
-imdaemon.py
+"""daemon-Eyes
+
+Disk And Execution MONitor
+
+This is meant to be as pythonic as possible to illustrate daemonization
+and functionality / termination within standard process state codes.
+
+Bits and pieces taken from the watch rack for changes script as well as http://code.activestate.com/recipes/278731-creating-a-daemon-the-python-way/
+
 """
 import argparse
 import logging
@@ -16,7 +22,7 @@ if os.geteuid() != 0:
     print "I'm a daemon, I need root / sudo. \n \n Exiting."
     sys.exit(1)
     """
-# Default umask creation
+# Default umask / file  mode creation mask of the daemon.
 UMASK = 0
 
 # Default working dir
@@ -31,26 +37,51 @@ if (hasattr(os, "devnull")):
 else:
     REDIRECT_TO = "/dev/null"
 # Begin daemonization
-def main():
+def daemon():
+    """Detach a process from the controlling terminal and run it in the background as a true daemon"""
     try:
-        if args.logfile:
-            log_to_file = logging.FileHandler(args,logfile)
-            log_to_file.setLevel(logging.DEBUG)
-            log_to_file.setFormatter(formatter)
-            logger.addHandler(log_to_file)
-            logger.info("startup")
-        else:
-            logger.addHandler(log_to_console)
-            logger.debug("Startup")
+        # Fork child process, and returns control to shell
+        # This guaranteed that the child will not be a process group leader (parent)
+        pid = os.fork()
+    except OSError, e:
+        raise Exception, "%s [%d]" % (e.strerror,e.errno)
+    if (pid == 0):   # First Child Process
+        os.setsid()
+        """
+        setsid() creates a new session if the calling process is not a process group leader.
+        The calling process is the leader of the new session, the process group leader of the new process group, and
+        has no controlling terminal. The process group ID and session ID of the calling process are set to the PID of the calling process.
+        The calling process will be the only process in this new process group and in this new session.
+        """
 
-    except IOError as e:
-        logger.critical("Error trying to open {} error({}): {}".format(args.logfile,e.errno,e.strerror))
+        try:
+            # Fork a second child process, and exit immediately to prevent zombie processes.
+            pid = os.fork() # Fork second child
+        except OSError, e:
+            raise Exception, "%s [%d]" % (e.strerror, e.errno)
+
+        if (pid == 0):
+            os.chdir(WORKDIR)
+            os.umask(UMASK)
+
+        else:
+                os._exit(0) # Exit Parent (the first child) of the second child.
+    else:
+        print "PID is currently registering as ",pid
 # CLI Argument Parsing
 parser = argparse.ArgumentParser(description = 'This is a light weight daemon to demonstrate a syslog server / daemon over UDP which inherits from a syslog client.')
 parser.add_argument('-help', action='help', help="Show this help message, and exit.")
 parser.add_argument('--logfile', help='Path to the logfile. May not be useful when using the --verbose flag.')
 parser.add_argument('-v', help='Increases Verbosity of the script / daemon. Look for more redirection to syslog as well.', action='store_true')
-args = parser.parse_args()
+if len(sys.argv) < 2:
+    parser.print_usage()
+    #parser.print_help() # More verbose output for non args.
+    sys.exit(1)
+else:
+    # Initialize the command-line arguments dictionary, and populate $
+    #
+    args = parser.parse_args()
+
 
 # Set basic logging config.
 logging.basicConfig(level=logging.INFO, format='%(message)s', datefmt='', filename=LOG_FILE, filemode = 'a')
@@ -75,4 +106,4 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 #
 log_to_console.setFormatter(formatter)
 
-main()
+daemon()
